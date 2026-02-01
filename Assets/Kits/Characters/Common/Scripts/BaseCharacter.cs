8aa207b090ec326dab36344df67daf49c7e6d37b
Quique Cores Rodríguez <15905530+QuiqueCores@users.noreply.github.com>
2026-02-01 18:25:38 +0100

    feat(characters): update animation parameters in BaseCharacter

--- Assets/Kits/Characters/Common/Scripts/BaseCharacter.cs
+++ Assets/Kits/Characters/Common/Scripts/BaseCharacter.cs
@@ -5,19 +5,24 @@ public class BaseCharacter : MonoBehaviour
     [SerializeField] float linearSpeed = 1f;
 
     Rigidbody2D rb2D;
+    Animator animator;
 
     protected virtual void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     protected virtual void Update()
     {
-
+        animator.SetFloat("HorizontalVelocity", lastMoveDirection.x);
+        animator.SetFloat("VerticalVelocity", lastMoveDirection.y);
     }
 
+    Vector2 lastMoveDirection;
     protected void Move(Vector2 direction)
     {
         rb2D.position += direction * linearSpeed * Time.deltaTime;
+        lastMoveDirection = direction;
     }
 }
