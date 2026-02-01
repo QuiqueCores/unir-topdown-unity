aabe5a8c43f08da9c560537164ddc70734323b1f
Quique Cores Rodríguez <15905530+QuiqueCores@users.noreply.github.com>
2026-02-01 19:03:58 +0100

    feat(player): add ability to punch

--- Assets/Kits/Characters/Player/Scripts/PlayerCharacter.cs
+++ Assets/Kits/Characters/Player/Scripts/PlayerCharacter.cs
@@ -4,6 +4,11 @@ using UnityEngine.InputSystem;
 public class PlayerCharacter : BaseCharacter
 {
     [SerializeField] InputActionReference move;
+    [SerializeField] InputActionReference punch;
+
+    [Header("Punch")]
+    [SerializeField] float punchRadius = 0.3f;
+    [SerializeField] float punchRange = 1.0f;
 
     protected override void Awake()
     {
@@ -17,6 +22,9 @@ public class PlayerCharacter : BaseCharacter
         move.action.started += OnMove;
         move.action.performed += OnMove;
         move.action.canceled += OnMove;
+
+        punch.action.Enable();
+        punch.action.performed += OnPunch;
     }
 
     protected override void Update()
@@ -24,6 +32,12 @@ public class PlayerCharacter : BaseCharacter
         base.Update();
 
         Move(rawMove);
+
+        if (mustPunch)
+        {
+            mustPunch = false;
+            PerformPunch();
+        }
     }
 
     private void OnDisable()
@@ -33,11 +47,46 @@ public class PlayerCharacter : BaseCharacter
         move.action.started -= OnMove;
         move.action.performed -= OnMove;
         move.action.canceled -= OnMove;
+
+        punch.action.Disable();
+        punch.action.performed -= OnPunch;
     }
 
     Vector2 rawMove;
     private void OnMove(InputAction.CallbackContext context)
     {
         rawMove = context.action.ReadValue<Vector2>();
+
+        if (rawMove.magnitude > 0f)
+        {
+            punchDirection = rawMove.normalized;
+        }
+    }
+
+    bool mustPunch;
+    private void OnPunch(InputAction.CallbackContext context)
+    {
+        mustPunch = true;
+    }
+
+    Vector2 punchDirection = Vector2.down;
+    private void PerformPunch()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, punchRadius, punchDirection * punchRange);
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            BaseCharacter otherBaseCharacter = hit.collider.GetComponent<BaseCharacter>();
+            if (otherBaseCharacter != this)
+            {
+                otherBaseCharacter?.NotifyPunch();
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, punchDirection * punchRange);
     }
 }
