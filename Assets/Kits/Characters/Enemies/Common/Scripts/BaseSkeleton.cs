c2f0b73705cc5d89763d7b2fe86a76e2b1986291
Quique Cores Rodríguez <15905530+QuiqueCores@users.noreply.github.com>
2026-02-01 13:12:09 +0100

    feat(enemy): add BaseSkeleton script with sight and move to target

---
+++ Assets/Kits/Characters/Enemies/Common/Scripts/BaseSkeleton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BaseSkeleton : BaseCharacter
+{
+    Sight2D sight;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        sight = GetComponent<Sight2D>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        Transform closestTarget = sight.GetClosestTarget();
+        if (closestTarget != null)
+        {
+            Move((closestTarget.position - transform.position).normalized);
+        }
+    }
+}
