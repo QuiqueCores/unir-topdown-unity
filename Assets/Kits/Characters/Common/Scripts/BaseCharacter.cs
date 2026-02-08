28d116a28a8ac86498ddafd8abc1d6f7562748a9
Enrique A. Cores Rodríguez <15905530+QuiqueCores@users.noreply.github.com>
2026-02-08 13:12:03 +0100

    feat(characters): implement IVisible2D in BaseCharacter

--- Assets/Kits/Characters/Common/Scripts/BaseCharacter.cs
+++ Assets/Kits/Characters/Common/Scripts/BaseCharacter.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 
-public class BaseCharacter : MonoBehaviour
+public class BaseCharacter : MonoBehaviour, IVisible2D
 {
     [SerializeField] float linearSpeed = 1f;
+
+    [SerializeField] int priority = 0;
+    [SerializeField] IVisible2D.Side side;
 
     Rigidbody2D rb2D;
     Animator animator;
@@ -29,5 +32,15 @@ public class BaseCharacter : MonoBehaviour
     internal void NotifyPunch()
     {
         Destroy(gameObject);
+    }
+
+    int IVisible2D.GetPriority()
+    {
+        return priority;
+    }
+
+    IVisible2D.Side IVisible2D.GetSide()
+    {
+        return side;
     }
 }
