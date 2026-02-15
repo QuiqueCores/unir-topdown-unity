15cbbfa2d9936236d856f9f3080093618b6baaba
Fiunc <anarodriguezlopez831@gmail.com>
2026-02-15 10:24:22 +0100

    Health-death-and-respawn-logic

--- Assets/Kits/Characters/Enemies/Common/Scripts/BaseSkeleton.cs
+++ Assets/Kits/Characters/Enemies/Common/Scripts/BaseSkeleton.cs
@@ -1,13 +1,45 @@
 using UnityEngine;
+using System.Collections;
 
-public class BaseSkeleton : BaseEnemy
+public class BaseSkeleton : BaseCharacter, IAttacker
 {
-    protected override void ExecuteAI()
+    Sight2D sight;
+    //protected float distancePlayer;
+    //[SerializeField] float blinkInterval = 0.2f;
+    MeleeAttack melee;
+
+    [Header("Attack")]
+    [SerializeField] int damage = 1;
+    [SerializeField] float attackRange = 0.8f;
+
+    public int Damage => damage;
+
+
+    protected override void Awake()
     {
-        Transform target = sight.GetClosestTarget();
-        if (target != null)
-        {
-            Move((target.position - transform.position).normalized);
-        }
+        base.Awake();
+        sight = GetComponent<Sight2D>();
+        melee = GetComponent<MeleeAttack>();
     }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        Transform target = sight.GetClosestTarget();
+        if (target == null)
+            return;
+
+        //Move((closestTarget.position - transform.position).normalized);
+        //distancePlayer = transform.position - closestTarget.position;
+        Vector2 dir = target.position - transform.position;
+        Move(dir.normalized);
+
+        if (dir.magnitude <= attackRange)
+        {
+            melee.TryAttack(dir.normalized);
+        }
+
+    }
+  
 }
