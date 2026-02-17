650d15b7f66397a28482e5df73061546c8e2e219
lucadev-itsathem <laura.devesa1996@gmail.com>
2026-02-17 01:23:48 +0100

    refactor(enemy): move core perception and movement logic to BaseEnemy

--- Assets/Kits/Characters/Enemies/Common/Scripts/BaseSkeleton.cs
+++ Assets/Kits/Characters/Enemies/Common/Scripts/BaseSkeleton.cs
@@ -1,45 +1,13 @@
 using UnityEngine;
 using System.Collections;
 
-public class BaseSkeleton : BaseCharacter, IAttacker
+public class BaseSkeleton : BaseEnemy
 {
-    Sight2D sight;
-    //protected float distancePlayer;
-    //[SerializeField] float blinkInterval = 0.2f;
-    MeleeAttack melee;
-
-    [Header("Attack")]
-    [SerializeField] int damage = 1;
-    [SerializeField] float attackRange = 0.8f;
-
-    public int Damage => damage;
-
-
-    protected override void Awake()
+    protected override void ExecuteAI()
     {
-        base.Awake();
-        sight = GetComponent<Sight2D>();
-        melee = GetComponent<MeleeAttack>();
+        base.ExecuteAI();
+        Move(distance.normalized);
     }
 
-    protected override void Update()
-    {
-        base.Update();
 
-        Transform target = sight.GetClosestTarget();
-        if (target == null)
-            return;
-
-        //Move((closestTarget.position - transform.position).normalized);
-        //distancePlayer = transform.position - closestTarget.position;
-        Vector2 dir = target.position - transform.position;
-        Move(dir.normalized);
-
-        if (dir.magnitude <= attackRange)
-        {
-            melee.TryAttack(dir.normalized);
-        }
-
-    }
-  
 }
