7fd57908442be1725b7187201bc32913b301583b
lucadev-itsathem <laura.devesa1996@gmail.com>
2026-02-17 17:10:32 +0100

    feat(inventory): add InventoryItemDefinition ScriptableObject

---
+++ Assets/Kits/GameplayObjects/Common/Scripts/InventoryItemDefinition.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item Definition", menuName = "Inventory/Item Definition")]
+public class InventoryItemDefinition : ScriptableObject
+{
+    public string uniqueItemName;
+}
