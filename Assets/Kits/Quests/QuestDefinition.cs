c2ebcc1cc0583243493b0364c92442a7b081d0cd
lucadev-itsathem <laura.devesa1996@gmail.com>
2026-02-19 21:18:39 +0100

    feat(quest): create QuestDefinition container for designer use

---
+++ Assets/Kits/Quests/QuestDefinition.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "New Quest", menuName = "Quests/Quest Definition")]
+public class QuestDefinition : ScriptableObject
+{
+    public string questName;
+    [TextArea] public string questStory;
+
+    [Header("Objectives")]
+    public List<QuestObjective> objectives;
+
+    [Header("Rewards")]
+    public int experienceReward;
+    public ItemDefinitionSO itemReward;
+}
