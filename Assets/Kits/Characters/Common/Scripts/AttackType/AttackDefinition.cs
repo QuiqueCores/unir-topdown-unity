8a143fd38fe435f28e9caf0391b399bb4d16a869
ccsalazarj <ccsalazarj@gmail.com>
2026-02-20 19:46:23 +0100

    feat(Scriptable object): adding so por melee attack

---
+++ Assets/Kits/Characters/Common/Scripts/AttackType/AttackDefinition.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AttackDefinition : ScriptableObject
+{
+    public float attackRadius;
+    public float attackCooldown;
+    public float attackRange;
+}
